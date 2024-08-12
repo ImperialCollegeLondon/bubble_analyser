a6ee25b073c0472bc2233f6a8d0dc2cfbd5fb400
Yiyang Guan <eeeyoung2001@gmail.com>
2024-08-13 01:23:35 +0800

    Finish the basic function of default algorithm, see docstring for more details

--- bubble_analyser/default.py
+++ bubble_analyser/default.py
@@ -1,54 +1,50 @@
 import tomllib
 from pprint import pprint
 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import ndimage
+from calculate_circle_properties import calculate_circle_properties
+from calculate_px2cm import calculate_px2cm
+from image_preprocess import image_preprocess
+from morphological_process import morphological_process
 from skimage import (
     color,
-    filters,
     io,
-    measure,
     morphology,
-    segmentation,
     transform,
 )
-from skimage.io import imsave
+from threshold import threshold
 
 
-def extendedmin(img, H):
-    mask = img.copy()
-    marker = mask + H
-    hmin = morphology.reconstruction(marker, mask, method="erosion")
-    return morphology.local_minima(hmin)
+def load_image(image_path: str, img_resample: float) -> tuple[np.ndarray, np.ndarray]:
+    """Read and preprocess the input image.
 
+    This function loads an image from the specified path, resizes it according to the
+    given resampling factor, and converts it to grayscale if the image is in RGB format.
 
-def imposemin(img, minima):
-    marker = np.full(img.shape, np.inf)
-    marker[minima == 1] = 0
-    mask = np.minimum((img + 1), marker)
-    return morphology.reconstruction(marker, mask, method="erosion")
+    Args:
+        image_path: The file path of the image to load.
+        img_resample: The factor by which the image will be resampled (e.g., 0.5 for reducing the size by half).
 
-
-def default(image_path, params):
+    Returns:
+        A tuple containing:
+        - The preprocessed grayscale image (if the original was in RGB) or the original
+        grayscale image.
+        - The resized image in RGB format.
+    """
     # Read the input image
     img = io.imread(image_path)
 
-    # Extract parameters from the params dictionary
-    se = morphology.disk(
-        params["Morphological_element_size"]
-    )  # Structuring element for morphological operations
-    nb = params["Connectivity"]  # Neighborhood connectivity (4 or 8)
-    marker_size = params["Marker_size"]  # Marker size for watershed segmentation
-    px2mm = params["px2mm"]  # Image resolution conversion factor (pixels to mm)
-    img_resample = params["resample"]  # Resampling factor for image resizing
-    bknd_img_path = params["background_img"]  # Path to the background image
-    E_max = params["Max_Eccentricity"]  # Maximum eccentricity threshold
-    S_min = params["Min_Solidity"]  # Minimum solidity threshold
-    Dmin = params["min_size"]  # Minimum bubble size (in mm)
-    do_batch = params["do_batch"]  # Flag for batch processing
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    scale_percent = img_resample * 100  # percent of original size
+    width = int(imgRGB.shape[1] * scale_percent / 100)
+    height = int(imgRGB.shape[0] * scale_percent / 100)
+    dim = (width, height)
 
-    # Resize the image for faster processing
+    # resize image
+    imgRGB = cv2.resize(imgRGB, dim, interpolation=cv2.INTER_AREA)
+
     img = transform.resize(
         img,
         (int(img.shape[0] * img_resample), int(img.shape[1] * img_resample)),
@@ -57,105 +53,148 @@ def default(image_path, params):
     if img.ndim > 2:
         img = color.rgb2gray(img)  # Convert to grayscale if the image is in RGB
 
-    # Use background image for thresholding if provided
-    # if bknd_img_path is not None:
-    #     bknd_img = io.imread(bknd_img_path)
-    #     if bknd_img.ndim > 2:
-    #         bknd_img = color.rgb2gray(bknd_img)  # Convert background image to grayscale
-    #     T = filters.threshold_local(bknd_img, 3)  # Local threshold for background image+-
-    #     imsave('T.png', T.astype(np.uint8) * 255)
-    #     BW = img > transform.rescale(T, img_resample)  # Binary image using background threshold
-    #     # BW = img > T
-    # else:
-
-    BW = img > filters.threshold_otsu(img)  # Binary image using Otsu's thresholding
-    imsave("tests/BW_image.png", BW.astype(np.uint8) * 255)
-
-    # Perform morphological closing and fill holes
-    B = morphology.closing(~BW, se)
-    imsave("tests/BW_closing.png", B.astype(np.uint8) * 255)
-    B = ndimage.binary_fill_holes(B)
-    imsave("tests/BW_binaryfill.png", B.astype(np.uint8) * 255)
-    B = segmentation.clear_border(B)
-    imsave("tests/BW_clearborder.png", B.astype(np.uint8) * 255)
-
-    # Use distance transform and watershed for segmentation
-    R = -ndimage.distance_transform_edt(~B)
-    print(R)
-    R2 = morphology.h_minima(-R, marker_size)
-    Ld2 = segmentation.watershed(R2, connectivity=nb)
-    B[Ld2 == 0] = 0
-    imsave("tests/BW_after_R2_watershed.png", B.astype(np.uint8) * 255)
-
-    CH = morphology.convex_hull_image(B)
-    R3 = -ndimage.distance_transform_edt(~CH)
-
-    mask = extendedmin(R3, nb)
-    R4 = imposemin(R3, mask)
-    Ld3 = segmentation.watershed(R4, connectivity=nb)
-    CH[Ld3 == 0] = 0
-
-    imsave("tests/R.png", R.astype(np.uint8) * 255)
-    imsave("tests/R2.png", R2.astype(np.uint8) * 255)
-    imsave("tests/R3.png", R3.astype(np.uint8) * 255)
-    imsave("tests/Ld2.png", Ld2.astype(np.uint8) * 255)
-
-    # Fix peak_local_max and watershed usage
-
-    # Label the connected components and calculate geometric properties
-    CC = measure.label(CH, connectivity=4)
-    props = measure.regionprops(CC, "equivalent_diameter", "eccentricity", "solidity")
-
-    # Extract properties and apply thresholds
-    E = np.array([prop.eccentricity for prop in props])
-    D = np.array([prop.equivalent_diameter for prop in props])
-    S = np.array([prop.solidity for prop in props])
-    D = D * px2mm / img_resample
-
-    # Filter out abnormal bubbles
-    idx = (E >= E_max) | (S <= S_min) | (D < Dmin)
-    D = D[~idx]
-
-    # Collect extra information about bubbles
-    extra_info = {"Eccentricity": E[~idx], "Solidity": S[~idx]}
-
-    # Create a labeled image if not doing batch processing
-    if do_batch:
-        L_image = None
-    else:
-        allowableAreaIndexes = ~idx
-        keeperIndexes = np.where(allowableAreaIndexes)[0]
-        keeperBlobsImage = np.isin(measure.label(CH), keeperIndexes + 1)
-        keeperBlobsImage = transform.resize(
-            keeperBlobsImage, (img.shape[0], img.shape[1]), anti_aliasing=True
-        )
-        L_image = measure.label(keeperBlobsImage, connectivity=nb)
-
-    return D, L_image, extra_info
+    return img, imgRGB
 
 
-def load_toml(file_path) -> dict:
-    """Load TOML data from file"""
+def load_toml(file_path: str) -> dict[str, str | int | float]:
+    """Load configuration parameters from a TOML file.
+
+    This function reads the TOML configuration file from the specified path and loads
+    its contents into a dictionary.
+
+    Args:
+        file_path: The file path of the TOML configuration file.
+
+    Returns:
+        A dictionary containing the configuration parameters from the TOML file.
+    """
     with open(file_path, "rb") as f:
-        toml_data: dict = tomllib.load(f)
+        toml_data: dict[str, str | int | float] = tomllib.load(f)
         return toml_data
 
 
-if __name__ == "__main__":
+def run_algorithm(
+    target_img: np.ndarray,
+    bknd_img: np.ndarray,
+    imgRGB: np.ndarray,
+    params: dict,
+    px2cm: float,
+    threshold_value: float,
+) -> None:
+    """Execute the image processing algorithm on the target image.
+
+    This function performs a series of image processing steps on the target image,
+    including thresholding, morphological processing, and watershed segmentation.
+    It then calculates properties of the detected circular features, such as equivalent
+    diameter and area, in centimeters using the provided pixel-to-centimeter ratio.
+
+    Args:
+        target_img: The preprocessed target image.
+        bknd_img: The background image used for thresholding.
+        imgRGB: The resized target image in RGB format.
+        params: A dictionary of parameters loaded from the TOML file.
+        px2cm: The conversion factor between pixels and centimeters.
+
+    Returns:
+        None. The function displays and saves various intermediary and final images, and
+        prints the properties of the detected circular features.
+    """
+    # Extract parameters from the dictionary
+    element_size = morphology.disk(
+        params["Morphological_element_size"]
+    )  # Structuring element for morphological operations
+    connectivity = params["Connectivity"]  # Neighborhood connectivity (4 or 8)
+    marker_size = params["Marker_size"]  # Marker size for watershed segmentation
+    max_eccentricity = params["Max_Eccentricity"]  # Maximum eccentricity threshold
+    min_solidity = params["Min_Solidity"]  # Minimum solidity threshold
+    min_bubble_size = params["min_size"]  # Minimum bubble size (in mm)
+    do_batch = params["do_batch"]  # Flag for batch processing
+
+    # Display the original image
+    plt.figure()
+    plt.subplot(231)
+    plt.title("1. Original image")
+    plt.imshow(target_img, cmap="gray")
+
+    # Apply thresholding and morphological processing
+    plt.subplot(232)
+    imgThreshold = threshold(target_img, bknd_img, threshold_value)
+    imgThreshold = morphological_process(imgThreshold, element_size)
+    plt.title("2. Thresh&morph process")
+    plt.imshow(imgThreshold * 255, cmap="gray")
+
+    # Apply distance transform
+    plt.subplot(233)
+    distTrans = cv2.distanceTransform(imgThreshold, cv2.DIST_L2, 5)
+    plt.title("3. Distance Transform")
+    plt.imshow(distTrans)
+
+    # Apply thresholding to the distance transform
+    plt.subplot(234)
+    _, distThresh = cv2.threshold(
+        distTrans, 0.3 * distTrans.max(), 255, cv2.THRESH_BINARY
+    )
+    plt.title("4. Threshold of distTrans")
+    plt.imshow(distThresh)
+
+    # Apply connected component labeling
+    plt.subplot(235)
+    distThresh = np.uint8(distThresh)
+    _, labels = cv2.connectedComponents(distThresh)
+    plt.title("5. Labels")
+    plt.imshow(labels)
+
+    # Apply watershed segmentation
+    plt.figure()
+    plt.subplot(121)
+    labels = np.int32(labels)
+    labels = cv2.watershed(imgRGB, labels)
+    plt.title("6. Final graph after watershed")
+    plt.imshow(labels)
+
+    # Display the images
+    plt.show()
+
+    # Calculate and print the circle properties
+    circle_properties = calculate_circle_properties(labels, px2cm)
+    pprint(circle_properties)
+
+
+def default() -> None:
+    """Run the default image processing routine.
+
+    This function loads the configuration parameters from the TOML file, calculates the
+    pixel-to-centimeter ratio using a reference ruler image, and then runs the image
+    processing algorithm on the target image to detect and analyze circular features.
+
+    Args:
+        None.
+
+    Returns:
+        None. The function orchestrates the loading of images, execution of the algorithm,
+        and display of results.
+    """
     # Load parameters from the TOML configuration file
     params = load_toml("./bubble_analyser/config.toml")
-    pprint(params)
 
-    # Path to the input image
-    image_path = "./tests/sample_images/03.jpg"
+    # Read path and image resample factor
+    ruler_img_path: str = params.get("ruler_img", "")
+    target_img_path: str = params.get("target_img", "")
+    bknd_img_path: str = params.get("background_img", "")
+    img_resample_factor: float = params.get("resample", 0.0)
+    threshold_value: float = params.get("threshold_value", 0.0)
+
+    # Calculate the pixel to cm ratio
+    px2cm = calculate_px2cm(ruler_img_path, img_resample_factor)
+    print(f"Pixel to cm ratio: {px2cm} cm/pixel")
+
+    # Read the background and target image, resize and process into gray scale
+    bknd_img, _ = image_preprocess(bknd_img_path, img_resample_factor)
+    target_img, imgRGB = image_preprocess(target_img_path, img_resample_factor)
 
     # Run the default image processing algorithm
-    D, L_image, extra_info = default(image_path, params)
+    run_algorithm(target_img, bknd_img, imgRGB, params, px2cm, threshold_value)
 
-    # Print the diameters of detected bubbles
-    print("Diameters (mm):", D)
 
-    # Display the labeled image if it exists
-    if L_image is not None:
-        plt.imshow(L_image)
-        plt.show()
+if __name__ == "__main__":
+    default()
