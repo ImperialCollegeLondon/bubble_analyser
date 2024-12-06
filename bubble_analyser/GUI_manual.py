52bae39f10d53143cf6a0eca16f39edf617571fc
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-12-06 06:43:43 +0000

    :wrench: Import missing system dependencies in Linux.

--- bubble_analyser/GUI_manual.py
+++ bubble_analyser/GUI_manual.py
@@ -13,7 +13,7 @@ Classes:
 
 """
 
-import csv  # noqa: I001
+import csv
 import os
 import sys
 import time
@@ -22,6 +22,7 @@ from pathlib import Path
 
 import numpy as np
 import toml as tomllib
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from numpy import typing as npt
 from PySide6.QtCore import Qt, QThread, Signal
@@ -50,9 +51,6 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 from skimage import morphology
-
-from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
-
 
 from .calculate_px2mm import calculate_px2mm
 from .config import Config
