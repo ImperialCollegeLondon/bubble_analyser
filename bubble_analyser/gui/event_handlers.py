6d1522d0697639a59bd42ca4377a0badc970cde0
Yiyang <eeeyoung2001@gmail.com>
2025-04-02 19:08:56 +0100

    Fix csv export parses

--- bubble_analyser/gui/event_handlers.py
+++ bubble_analyser/gui/event_handlers.py
@@ -1512,7 +1512,9 @@ class ResultsTabHandler(QThread):
         self.gui.histogram_canvas.fig.savefig(graph_path)
 
         headers = [
-            "major_axis_lengthminor_axis_lengthequivalent_diameter",
+            "major_axis_length",
+            "minor_axis_length",
+            "equivalent_diameter",
             "area",
             "perimeter",
             "eccentricity",
